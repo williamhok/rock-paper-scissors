95da3fd6f2f30142109e52bfa6ed6e950dd7ea18
william <173856982+williamhok@users.noreply.github.com>
2024-06-27 15:44:41 +1000

    Add playRound()

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     const comChoices = ["rock", "paper", "scissors"]
     let random = Math.floor(Math.random() * comChoices.length)
     return comChoices[random]
 }
-
-console.log(getComputerChoice())
 
 function getHumanChoice() {
     const humChoice = prompt("Rock, Paper, or Scissor?")
@@ -12,4 +13,37 @@ function getHumanChoice() {
     return humChoice
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    console.log("Human: " + humanChoice)
+    console.log("Computer: " + computerChoice)
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log("Draw")
+        } else if (computerChoice == "paper") {
+            console.log("Computer win")
+        } else {
+            console.log("You win")
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("You win")
+        } else if (computerChoice == "paper") {
+            console.log("Draw")
+        } else {
+            console.log("Computer win")
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("Computer win")
+        } else if (computerChoice == "paper") {
+            console.log("You win")
+        } else {
+            console.log("Draw")
+        }
+    }
+}
+
+const humanSelection = getHumanChoice().toLowerCase()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
