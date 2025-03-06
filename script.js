d9ddc94fc382bd800e31fede30aeb69a59b63178
william <201534572+williamhok@users.noreply.github.com>
2025-03-07 09:18:35 +1100

    Added function when human and the computer pick rock

--- script.js
+++ script.js
@@ -8,18 +8,44 @@ function getRandomInt(max) {
 function getComputerChoice() {
     let computerChoice = getRandomInt(3)
     if (computerChoice == 0) {
+        computerChoice = "rock"
         console.log("rock")
     } else if (computerChoice == 1) {
+        computerChoic = "paper"
         console.log("paper")
     } else {
+        computerChoice = "scissors"
         console.log("scissors")
     }
+
+    return computerChoice
 }
 
 function getHumanChoice() {
     let humanChoice = window.prompt("Rock Paper Scissors")
-    getComputerChoice()
     console.log(humanChoice)
+
+    return humanChoice
 }
 
-getHumanChoice()
+function playround(humanChoice, computerChoice) {
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log("Draw")
+        } else if (computerChoice == "paper") {
+            console.log("You lose! Paper beats Rock")
+        } else if (computerChoice == "scissors") {
+            console.log("You win! Rock beats Scissors")
+        }
+    } else if (humanChoice == "paper") {
+        console.log(humanChoice)
+    } else if (humanChoice == "scissors") {
+        console.log(humanChoice)
+
+    }
+}
+
+const humanSelection = getHumanChoice().toLowerCase()
+const computerSelection = getComputerChoice()
+
+playround(humanSelection, computerSelection)
